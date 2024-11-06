corrected the timing pdf normalization

flux/create_observables.py
@@ -33,11 +33,14 @@ def create_observables(flux, params) -> dict:
     
     observables = {}
 
+    tmp = 0
     for flavor in flux.keys():
-        num_neutrinos = params["detector"]["nus_per_pot"][flavor] * params["detector"]["pot"] / (4 * np.pi * np.power(params["detector"]["distance"], 2))
+
+        # flux is normalized to number of neutrinos per POT so it's not counted here
+        num_neutrinos = params["detector"]["pot"] / (4 * np.pi * np.power(params["detector"]["distance"], 2))
 
         # Load in energy and calculate observable energy before efficiencies
-        truth_level_energy = np.dot(flux_matrix, flux[flavor]["energy"][1][1:60])
+        truth_level_energy = np.dot(flux_matrix, flux[flavor]["energy"][1])
         observable_energy = np.dot(np.nan_to_num(detector_matrix), truth_level_energy)
         dx = params["detector"]["detector_matrix_dx"]
         observable_bin_arr = np.arange(0, len(observable_energy) * dx, dx)
@@ -65,7 +68,6 @@ def create_observables(flux, params) -> dict:
             "time": (flux[flavor]["time"][0], np.dot(time_matrix, truth_level_time) * N_true * e_frac)
         }
 
-        print("num neutrinos: ")
         print("Observed counts for ", flavor, ":", np.sum(observables[flavor]["energy"][1]), np.sum(observables[flavor]["time"][1]))
     
     return observables

flux/nuflux.py
@@ -4,16 +4,16 @@ import numpy as np
 from flux.probabilities import Pab, sin2theta
 
 
-def read_flux_from_root(filename: str) -> dict:
+def read_flux_from_root(params: dict) -> dict:
     """
     
-    filename: str
-        The name of the root file containing the flux information.
+    params: dictionary with flux_file key and other info :)
 
     returns: dictionary with SNS flux information
     
     """
 
+    filename = params["flux_file"]
     paper_rf = uproot.open(filename)
 
     convolved_energy_and_time_of_nu_mu = paper_rf["convolved_energy_time_of_nu_mu;1"]
@@ -25,7 +25,7 @@ def read_flux_from_root(filename: str) -> dict:
     NuE = convolved_energy_and_time_of_nu_e.values()
     anc_keNuE_edges = convolved_energy_and_time_of_nu_e.axis(1).edges()[1:60]
     anc_keNuE_values = np.sum(NuE, axis=0)[1:60]
-    anc_keNuE_values = anc_keNuE_values / np.sum(anc_keNuE_values)
+    anc_keNuE_values = (anc_keNuE_values / np.sum(anc_keNuE_values))*params["detector"]["nus_per_pot"]["nuE"]
     anc_tNuE_edges = convolved_energy_and_time_of_nu_e.axis(0).edges()
     anc_tNuE_values = np.sum(NuE, axis=1)
     anc_tNuE_values = anc_tNuE_values / np.sum(anc_tNuE_values)
@@ -34,7 +34,7 @@ def read_flux_from_root(filename: str) -> dict:
     NuEBar = convolved_energy_and_time_of_nu_e_bar.values()
     anc_keNuEBar_edges = convolved_energy_and_time_of_nu_e_bar.axis(1).edges()[1:60]
     anc_keNuEBar_values = np.sum(NuEBar, axis=0)[1:60]
-    anc_keNuEBar_values = anc_keNuEBar_values / np.sum(anc_keNuEBar_values)
+    anc_keNuEBar_values = (anc_keNuEBar_values / np.sum(anc_keNuEBar_values))*params["detector"]["nus_per_pot"]["nuEBar"]
     anc_tNuEBar_edges = convolved_energy_and_time_of_nu_e_bar.axis(0).edges()
     anc_tNuEBar_values = np.sum(NuEBar, axis=1)
     anc_tNuEBar_values = anc_tNuEBar_values / np.sum(anc_tNuEBar_values)
@@ -43,7 +43,7 @@ def read_flux_from_root(filename: str) -> dict:
     NuMu = convolved_energy_and_time_of_nu_mu.values()
     anc_keNuMu_edges = convolved_energy_and_time_of_nu_mu.axis(1).edges()[1:60]
     anc_keNuMu_values = np.sum(NuMu, axis=0)[1:60]
-    anc_keNuMu_values = anc_keNuMu_values / np.sum(anc_keNuMu_values)
+    anc_keNuMu_values = (anc_keNuMu_values / np.sum(anc_keNuMu_values))*params["detector"]["nus_per_pot"]["nuMu"]
     anc_tNuMu_edges = convolved_energy_and_time_of_nu_mu.axis(0).edges()
     anc_tNuMu_values = np.sum(NuMu, axis=1)
     anc_tNuMu_values = anc_tNuMu_values / np.sum(anc_tNuMu_values)
@@ -52,7 +52,7 @@ def read_flux_from_root(filename: str) -> dict:
     NuMuBar = convolved_energy_and_time_of_nu_mu_bar.values()
     anc_keNuMuBar_edges = convolved_energy_and_time_of_nu_mu_bar.axis(1).edges()[1:60]
     anc_keNuMuBar_values = np.sum(NuMuBar, axis=0)[1:60]
-    anc_keNuMuBar_values = anc_keNuMuBar_values / np.sum(anc_keNuMuBar_values)
+    anc_keNuMuBar_values = (anc_keNuMuBar_values / np.sum(anc_keNuMuBar_values))*params["detector"]["nus_per_pot"]["nuMuBar"]
     anc_tNuMuBar_edges = convolved_energy_and_time_of_nu_mu_bar.axis(0).edges()
     anc_tNuMuBar_values = np.sum(NuMuBar, axis=1)
     anc_tNuMuBar_values = anc_tNuMuBar_values / np.sum(anc_tNuMuBar_values)
@@ -152,6 +152,11 @@ def oscillate_flux(flux: dict) -> dict:
                 anti_oscillated_time_flux += np.sum(antitemp) / np.sum(flux[initial_antiflavor]["energy"][1]) * flux[initial_antiflavor]["time"][1]
             else:
                 anti_oscillated_time_flux += 0
+
+        if np.sum(oscillated_time_flux) != 0:
+            oscillated_time_flux /= np.sum(oscillated_time_flux)
+        if np.sum(anti_oscillated_time_flux) != 0:
+            anti_oscillated_time_flux /= np.sum(anti_oscillated_time_flux)
 
         oscillated_flux[final_flavor] = {
             "energy" : (flux[final_flavor]["energy"][0], oscillated_energy_flux),