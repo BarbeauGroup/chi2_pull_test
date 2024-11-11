737998030e6e677ff489476f33cad077f392db26
rbbouabid <ryan-bouabid@live.com>
2024-11-11 10:29:15 -0500

    poosh

--- stats/chi2.py
+++ stats/chi2.py
@@ -29,23 +29,23 @@ def chi2_stat(histograms: dict) -> float:
 
 
 
-# def chi2_sys(experiment, systematic_error_arr):
+def chi2_sys(experiment, systematic_error_arr):
 
 
-#     systematic_error_dict = experiment.get_systematic_error_dict()
+    systematic_error_dict = experiment.get_systematic_error_dict()
 
-#     chi2 = 0
-#     i = 0
-#     for key in systematic_error_dict:
+    chi2 = 0
+    i = 0
+    for key in systematic_error_dict:
         
-#         nuisance_param_value = systematic_error_arr[i]
-#         nuisance_param_error = systematic_error_dict[key]
+        nuisance_param_value = systematic_error_arr[i]
+        nuisance_param_error = systematic_error_dict[key]
 
-#         chi2 += (nuisance_param_value/nuisance_param_error)**2
+        chi2 += (nuisance_param_value/nuisance_param_error)**2
 
-#         i += 1
+        i += 1
     
-#     return chi2
+    return chi2
 
 
 # def chi2_total(experiment, toy_model, prediction_parameters, systematic_error_arr, systematic_error_dict):
