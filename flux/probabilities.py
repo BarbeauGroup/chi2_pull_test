505d810eb09d94fc83dc9612843dbf704ca4df8d
Charles Prior <charlie@charlieprior.com>
2024-11-11 23:28:12 -0500

    changed sin2theta to use U squared

--- flux/probabilities.py
+++ flux/probabilities.py
@@ -11,11 +11,11 @@ def sin2theta(alpha, beta, Ue4, Umu4, Utau4):
     beta : int
         +1 for e, +2 for mu, +3 for tau, +4 for sterile
     Ue4 : complex
-        Mixing matrix element Ue4.
+        Mixing matrix element Ue4 squared.
     Umu4 : complex
-        Mixing matrix element Umu4.
+        Mixing matrix element Umu4 squared.
     Utau4 : complex
-        Mixing matrix element Utau4.
+        Mixing matrix element Utau4 squared.
 
     Returns
     -------
@@ -24,13 +24,13 @@ def sin2theta(alpha, beta, Ue4, Umu4, Utau4):
     """
     if (alpha != 1 and alpha != 2 and alpha != 3 and alpha != 4) or (beta != 1 and beta != 2 and beta != 3 and beta != 4):
         raise ValueError("alpha and beta must be 1, 2, 3 or 4.")
-    Us4 = np.sqrt(1 - np.abs(Ue4)**2 - np.abs(Umu4)**2 - np.abs(Utau4)**2)
+    Us4 = 1 - Ue4 - Umu4 - Utau4
     delta = (1 if alpha == beta else 0)
     Ua4 = (Ue4 if alpha == 1 else Umu4 if alpha == 2 else Utau4 if alpha == 3 else Us4)
     Ub4 = (Ue4 if beta == 1 else Umu4 if beta == 2 else Utau4 if beta == 3 else Us4)
     return 4 * np.abs(
-        delta*Ua4*np.conjugate(Ub4)
-        - (np.abs(Ua4)*np.abs(Ub4))**2)
+        delta*Ua4
+        - Ua4*Ub4)
 
 def Pab(Enu, L, deltam41, alpha, beta, Ue4, Umu4, Utau4):
     """
