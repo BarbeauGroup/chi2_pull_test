b710938c56a813a356f388821515532833e55c44
rbbouabid <ryan-bouabid@live.com>
2024-11-12 12:07:32 -0500

    rebin bkd dict only once

--- steriles.py
+++ steriles.py
@@ -4,6 +4,7 @@ import corner
 from multiprocessing import Pool
 
 from utils.loadparams import load_params
+from utils.histograms import rebin_histogram
 from utils.data_loaders import read_flux_from_root, read_brns_nins_from_txt, read_data_from_txt
 from stats.fit import chi2_stat, chi2_sys
 
@@ -24,6 +25,27 @@ with open("flux/flux_dict.pkl", "rb") as f:
 params = load_params("config/csi.json")
 bkd_dict = read_brns_nins_from_txt(params)
 data_dict = read_data_from_txt(params)
+
+observable_bin_arr = np.asarray(params["analysis"]["energy_bins"])
+t_bin_arr = np.asarray(params["analysis"]["time_bins"])
+
+brn_e_weights = rebin_histogram(bkd_dict["brn"]["energy"][1], bkd_dict["brn"]["energy"][0], observable_bin_arr) / np.diff(observable_bin_arr)
+brn_t_weights = rebin_histogram(bkd_dict["brn"]["time"][1], bkd_dict["brn"]["time"][0], t_bin_arr) / np.diff(t_bin_arr)
+
+nin_e_weights = rebin_histogram(bkd_dict["nin"]["energy"][1], bkd_dict["nin"]["energy"][0], observable_bin_arr) / np.diff(observable_bin_arr)
+nin_t_weights = rebin_histogram(bkd_dict["nin"]["time"][1], bkd_dict["nin"]["time"][0], t_bin_arr) / np.diff(t_bin_arr)
+
+new_bkd_dict = {}
+new_bkd_dict["brn"] = {
+    "energy": np.asarray(brn_e_weights),
+    "time": np.asarray(brn_t_weights)
+}
+
+new_bkd_dict["nin"] = {
+    "energy": np.asarray(nin_e_weights),
+    "time": np.asarray(nin_t_weights)
+}
+
 
 def cost_function_global(x: np.ndarray) -> float:
     """
@@ -48,7 +70,7 @@ def cost_function_global(x: np.ndarray) -> float:
 
     oscillated_flux = oscillate_flux(flux=flux, oscillation_params=osc_params)
     osc_obs = create_observables(params=params, flux=oscillated_flux)
-    histograms_osc = analysis_bins(observable=osc_obs, bkd_dict=bkd_dict, data=data_dict, params=params, brn_norm=18.4, nin_norm=5.6)
+    histograms_osc = analysis_bins(observable=osc_obs, bkd_dict=new_bkd_dict, data=data_dict, params=params, brn_norm=18.4, nin_norm=5.6)
     
     return chi2_stat(histograms=histograms_osc, nuisance_params=nuisance_params) + chi2_sys(nuisance_params=nuisance_params, nuisance_param_priors=nuisance_param_priors)
 
