1445801b1d0a47afa96d9743ca8229666aeb19dd
Charles Prior <charlie@charlieprior.com>
2024-11-13 16:40:50 -0500

    ssb bins

---
+++ flux/ssb_pdf.py
@@ -0,0 +1,47 @@
+import numpy as np
+from utils.histograms import rebin_histogram, centers_to_edges
+
+def make_ssb_pdf(params: dict) -> dict:
+    """
+    params: dictionary with SSB information
+
+    returns: tuple with SSB PDF information
+    """
+
+    ssb_pdf = {}
+
+
+    # Energy PDF is just the AC data
+    energy_bins = np.asarray(params["analysis"]["energy_bins"])
+
+    dataBeamOnAC = np.loadtxt(params["detector"]["beam_ac_data_file"])
+    AC_PE = dataBeamOnAC[:,0]
+    AC_t = dataBeamOnAC[:,1]
+
+    # must filter the events that have t > time roi (e.g. 6)
+    AC_high_time_idx = np.where(AC_t > params["analysis"]["time_roi"][1])[0]
+    AC_PE = np.delete(AC_PE, AC_high_time_idx)
+
+    # and energy > energy roi (e.g. 60)
+    AC_high_energy_idx = np.where(AC_PE > params["analysis"]["energy_roi"][1])[0]
+    AC_PE = np.delete(AC_PE, AC_high_energy_idx)
+
+    AC_PE_hist, _ = np.histogram(AC_PE, bins=energy_bins)
+
+    ssb_pdf["energy"] = (energy_bins, AC_PE_hist)
+
+
+    # Time PDF is an analytic function
+    time_bins = np.asarray(params["analysis"]["time_bins"])
+    k = 0.0494
+    def exp_decay(t, k):
+        return k * np.exp(-k*t)
+    
+    t = np.linspace(0, 6, 100)
+    y = exp_decay(t, k)
+    y /= np.sum(y)
+    t_edges = centers_to_edges(t)
+
+    ssb_pdf["time"] = (time_bins, rebin_histogram(y, t_edges, time_bins)/np.diff(time_bins))
+
+
