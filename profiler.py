99dd27ef18aaf40ed335a9b4646014aa226bb116
Charles Prior <charlie@charlieprior.com>
2024-11-29 14:49:39 -0500

    paralllel etc. and new sin2theta

--- profiler.py
+++ profiler.py
@@ -1,5 +1,8 @@
 import iminuit
 from scipy.optimize import Bounds
+from multiprocessing import Pool
+from itertools import product
+import istarmap
 
 from tqdm import tqdm
 import numpy as np
@@ -47,20 +50,26 @@ detector_matrix = np.load(params["detector"]["detector_matrix"])
 matrix = detector_matrix @ flux_matrix
 
 
-def cost(x, mass, u, index):
+def cost(x, mass, u, index, u2=None, /):
     """
-    x is [other_u, time_offset, a_flux, a_brn, a_nin, a_ssb]
+    x is [time_offset, a_flux, a_brn, a_nin, a_ssb, other_u]
     index is 1 for Ue4, 2 for Umu4, 3 for Utau4
     """
 
-    time_offset = x[1]
+    time_offset = x[0]
 
     osc_params = [params["detector"]["distance"]/100., mass, 0, 0, 0.0]
     if index == 1:
         osc_params[2] = u
-        osc_params[3] = x[0]
+        if u2 is not None:
+            osc_params[3] = u2
+        else:
+            osc_params[3] = x[-1]
     elif index == 2:
-        osc_params[2] = x[0]
+        if u2 is not None:
+            osc_params[2] = u2
+        else:
+            osc_params[2] = x[-1]
         osc_params[3] = u
     else:
         raise ValueError("Invalid index")
@@ -69,17 +78,56 @@ def cost(x, mass, u, index):
     osc_obs = create_observables(params=params, flux=oscillated_flux, time_offset=time_offset, matrix=matrix, flavorblind=True)
     hist_osc = analysis_bins(observable=osc_obs, ssb_dict=ssb_dict, bkd_dict=bkd_dict, data=data_dict, params=params, ssb_norm=1286, brn_norm=18.4, nin_norm=5.6, time_offset=time_offset)
 
-
     nuisance_param_priors = [params["detector"]["systematics"]["flux"],
                             params["detector"]["systematics"]["brn"],
                             params["detector"]["systematics"]["nin"],
                             params["detector"]["systematics"]["ssb"]]
 
-    return -2 * (loglike_stat(hist_osc, x[2:]) + loglike_sys(x[2:], nuisance_param_priors))
+    return -2 * (loglike_stat(hist_osc, x[1:]) + loglike_sys(x[1:], nuisance_param_priors))
 
 
-u_bins = np.linspace(0, 1, 75)
-mass_bins = np.linspace(0, 50, 80)
+u_bins = np.linspace(0, 1, 50)
+mass_bins = np.linspace(0, 50, 50)
+
+def plot_sin2theta_new():
+    chi2 = np.load("chi2_parallel.npy")
+
+    sin2_bins = np.linspace(0, 1, len(u_bins))
+    sin2_chi2 = np.full((len(sin2_bins), len(mass_bins)), 1e6)
+    print(sin2_chi2.shape)
+
+    for i, u1 in enumerate(u_bins):
+        for j, u2 in enumerate(u_bins):
+            if u1 + u2 > 1:
+                continue
+            for k, mass in enumerate(mass_bins):
+                s2 = sin2theta(1, 2, u1, u2, 0)
+                idx = np.searchsorted(sin2_bins, s2)
+                if chi2[i, j, k] < sin2_chi2[idx, k]:
+                    sin2_chi2[idx, k] = chi2[i, j, k]
+    
+    fig, ax = plt.subplots()
+
+    sin2_chi2_ma = np.ma.masked_where(sin2_chi2 > 1e5, sin2_chi2)
+    xv, yv = np.meshgrid(sin2_bins, mass_bins)
+
+    # set background yellow
+    ax.set_facecolor('yellow')
+
+    ctr = ax.contourf(xv, yv, sin2_chi2_ma.T, algorithm='serial', levels=3, cmap='Purples', antialiased=True)
+    cbar = fig.colorbar(ctr)
+    cbar.ax.set_ylabel(r'$\Delta\chi^2$')
+
+    ax.set_xlabel(r"$\sin^22\theta_{e\mu}$")
+    ax.set_ylabel(r"$\Delta m^2_{41}$")
+
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    ax.set_xlim(10e-4, 1)
+    ax.set_ylim(10e-2, 50)
+
+    plt.show()
 
 def plot_sin2theta():
     chi2 = np.load("chi2.npy")
@@ -124,6 +172,45 @@ def plot_sin2theta():
 
     plt.show()
 
+def marginalize_mass_u(index):
+    chi2 = np.full((len(u_bins), len(mass_bins)), 1e6)
+    margin_u = np.zeros((len(u_bins), len(mass_bins)))
+
+    for i, u in tqdm(enumerate(u_bins)):
+        for j, mass in tqdm(enumerate(mass_bins), leave=False):
+            bounds = Bounds([-np.inf, -np.inf, -np.inf, -np.inf, -np.inf, 0], [np.inf, np.inf, np.inf, np.inf, np.inf, 1 - u], keep_feasible=True)
+            res = iminuit.minimize(cost, np.asarray([80, 0, 0, 0, 0, 0]), args=(mass, u, index), bounds=bounds)
+            chi2[i, j] = res.fun
+            margin_u[i, j] = res.x[0]
+            # print(f"mass: {mass}, u: {u}, chi2: {res.fun}, x: {res.x}, success: {res.success}")
+    
+    min_chi2 = np.min(chi2)
+    chi2 = chi2 - min_chi2
+    np.save("chi2.npy", chi2)
+    np.save("margin_u.npy", margin_u)
+
+def marginalize_mass_uu_helper(i, j, k):
+    u1 = u_bins[i]
+    u2 = u_bins[j]
+    mass = mass_bins[k]
+    if u1 + u2 > 1:
+        return (i, j, k, 1e6)
+    res = iminuit.minimize(cost, np.asarray([80, 0, 0, 0, 0]), args=(mass, u1, 1, u2))
+    return (i, j, k, res.fun)
+
+def marginalize_mass_uu():
+    param_grid = list(product(range(len(u_bins)), range(len(u_bins)), range(len(mass_bins))))
+
+    with Pool() as pool:
+        results = list(tqdm(pool.istarmap(marginalize_mass_uu_helper, param_grid), total=len(param_grid)))
+                       
+    chi2 = np.full((len(u_bins), len(u_bins), len(mass_bins)), 1e6)
+
+    for i, j, k, val in results:
+        chi2[i, j, k] = val
+        
+    chi2 = chi2 - np.min(chi2)
+    np.save("chi2_parallel.npy", chi2)
 
 def main(): 
     chi2 = np.full((len(u_bins), len(mass_bins)), 1e6)
@@ -177,4 +264,6 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    plot_sin2theta()
+    # plot_sin2theta()
+    # marginalize_mass_uu()
+    plot_sin2theta_new()
